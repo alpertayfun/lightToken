04cbfc4ada31184e4e9d50db890fd985cf6cfafe
Alper Tayfun <tayfun.alper@gmail.com>
2016-08-16 11:32:03 +0300

    security update

--- lightToken.js
+++ lightToken.js
@@ -83,139 +83,146 @@ try {
 						algorithm = options.algorithm;
 					}
 
-					var checkTime = optionsGet[3];
-					
+					var cipher3 = crypto.createCipher(algorithm3,key);
+					var crypted3 = cipher3.update(messageGet[1]+"."+JSON.stringify(options)+"."+optionsGet[2]+"."+optionsGet[3],'utf8','hex');
+					crypted3 += cipher3.final('hex');
 
-
-					if(checkTime=="0"){
+					if(crypted3==messageGet[2])
+					{
 						
+						var checkTime = optionsGet[3];
+
+						var cipher2 = crypto.createCipher(algorithm2,key);
+						var crypted2 = cipher2.update(messageGet[0],'utf8','hex');
+						crypted2 += cipher2.final('hex');
+
 						var decipher2 = crypto.createDecipher(algorithm2,key)
 						var dec2 = decipher2.update(optionsGet[0],'hex','utf8')
 						dec2 += decipher2.final('utf8');
 
-						var decipher = crypto.createDecipher(algorithm,key)
-						var dec = decipher.update(dec2,'hex','utf8')
-						dec += decipher.final('utf8');
-						callback({verify:JSON.parse(dec)});
-						return JSON.parse(dec);
-					}else{
 
-						if (expireCheck.some(function(e) { return checkTime.indexOf(e) >= 0; })) {
+						if(crypted2==messageGet[1])
+						{
+							if(checkTime=="0"){
+						
+									
+
+									var decipher = crypto.createDecipher(algorithm,key)
+									var dec = decipher.update(dec2,'hex','utf8')
+									dec += decipher.final('utf8');
+									callback({verify:JSON.parse(dec)});
+									return JSON.parse(dec);
+							}else{
+
+								if (expireCheck.some(function(e) { return checkTime.indexOf(e) >= 0; })) {
+									
+									var compare1 = new Date(optionsGet[2]);
+									var compare2 = new Date(optionsGet[2]);
+									
+									if(checkTime.indexOf("h") !== -1)
+									{
+										newCheckTime = checkTime.replace("h","");
+										compare2 = compare2.add({ hours: newCheckTime});
+										
+										if(Date.today().between(compare1,compare2))
+										{
+
+											var decipher = crypto.createDecipher(algorithm,key)
+											var dec = decipher.update(dec2,'hex','utf8')
+											dec += decipher.final('utf8');
+											callback({verify:JSON.parse(dec)});
+											return JSON.parse(dec);
+										}else{
+											callback({error:"auth Error"});
+											return "auth Error";
+										}
+									}
+
+									if(checkTime.indexOf("m") !== -1)
+									{
+										newCheckTime = checkTime.replace("m","");
+										compare2 = compare2.add({ minutes: newCheckTime});
+										
+										if(Date.today().between(compare1,compare2))
+										{
+
+											var decipher = crypto.createDecipher(algorithm,key)
+											var dec = decipher.update(dec2,'hex','utf8')
+											dec += decipher.final('utf8');
+											callback({verify:JSON.parse(dec)});
+											return JSON.parse(dec);
+										}else{
+											callback({error:"auth Error"});
+											return "auth Error";
+										}
+									}
+
+									if(checkTime.indexOf("days") !== -1)
+									{
+										newCheckTime = checkTime.replace("days","");
+										compare2 = compare2.add({ days: newCheckTime});
+										
+										if(Date.today().between(compare1,compare2))
+										{
+
+											var decipher = crypto.createDecipher(algorithm,key)
+											var dec = decipher.update(dec2,'hex','utf8')
+											dec += decipher.final('utf8');
+											callback({verify:JSON.parse(dec)});
+											return JSON.parse(dec);
+										}else{
+											callback({error:"auth Error"});
+											return "auth Error";
+										}
+									}
+
+									if(checkTime.indexOf("years") !== -1)
+									{
+										newCheckTime = checkTime.replace("years","");
+										compare2 = compare2.add({ years: newCheckTime});
+										
+										if(Date.today().between(compare1,compare2))
+										{
+
+											var decipher = crypto.createDecipher(algorithm,key)
+											var dec = decipher.update(dec2,'hex','utf8')
+											dec += decipher.final('utf8');
+											callback({verify:JSON.parse(dec)});
+											return JSON.parse(dec);
+										}else{
+											callback({error:"auth Error"});
+											return "auth Error";
+										}
+									}
+
+									if(checkTime.indexOf("s") !== -1)
+									{
+										newCheckTime = checkTime.replace("s","");
+										compare2 = compare2.add({ seconds: newCheckTime});
+										
+										if(Date.today().between(compare1,compare2))
+										{
+
+											var decipher = crypto.createDecipher(algorithm,key)
+											var dec = decipher.update(dec2,'hex','utf8')
+											dec += decipher.final('utf8');
+											callback({verify:JSON.parse(dec)});
+											return JSON.parse(dec);
+										}else{
+											callback({error:"auth Error"});
+											return "auth Error";
+										}
+									}
+								}
 							
-							var compare1 = new Date(optionsGet[2]);
-							var compare2 = new Date(optionsGet[2]);
-							
-							if(checkTime.indexOf("h") !== -1)
-							{
-								newCheckTime = checkTime.replace("h","");
-								compare2 = compare2.add({ hours: newCheckTime});
-								
-								if(Date.today().between(compare1,compare2))
-								{
-									var decipher2 = crypto.createDecipher(algorithm2,key)
-									var dec2 = decipher2.update(optionsGet[0],'hex','utf8')
-									dec2 += decipher2.final('utf8');
-
-									var decipher = crypto.createDecipher(algorithm,key)
-									var dec = decipher.update(dec2,'hex','utf8')
-									dec += decipher.final('utf8');
-									callback({verify:JSON.parse(dec)});
-									return JSON.parse(dec);
-								}else{
-									callback({error:"auth Error"});
-									return "auth Error";
-								}
 							}
-
-							if(checkTime.indexOf("m") !== -1)
-							{
-								newCheckTime = checkTime.replace("m","");
-								compare2 = compare2.add({ minutes: newCheckTime});
-								
-								if(Date.today().between(compare1,compare2))
-								{
-									var decipher2 = crypto.createDecipher(algorithm2,key)
-									var dec2 = decipher2.update(optionsGet[0],'hex','utf8')
-									dec2 += decipher2.final('utf8');
-
-									var decipher = crypto.createDecipher(algorithm,key)
-									var dec = decipher.update(dec2,'hex','utf8')
-									dec += decipher.final('utf8');
-									callback({verify:JSON.parse(dec)});
-									return JSON.parse(dec);
-								}else{
-									callback({error:"auth Error"});
-									return "auth Error";
-								}
-							}
-
-							if(checkTime.indexOf("days") !== -1)
-							{
-								newCheckTime = checkTime.replace("days","");
-								compare2 = compare2.add({ days: newCheckTime});
-								
-								if(Date.today().between(compare1,compare2))
-								{
-									var decipher2 = crypto.createDecipher(algorithm2,key)
-									var dec2 = decipher2.update(optionsGet[0],'hex','utf8')
-									dec2 += decipher2.final('utf8');
-
-									var decipher = crypto.createDecipher(algorithm,key)
-									var dec = decipher.update(dec2,'hex','utf8')
-									dec += decipher.final('utf8');
-									callback({verify:JSON.parse(dec)});
-									return JSON.parse(dec);
-								}else{
-									callback({error:"auth Error"});
-									return "auth Error";
-								}
-							}
-
-							if(checkTime.indexOf("years") !== -1)
-							{
-								newCheckTime = checkTime.replace("years","");
-								compare2 = compare2.add({ years: newCheckTime});
-								
-								if(Date.today().between(compare1,compare2))
-								{
-									var decipher2 = crypto.createDecipher(algorithm2,key)
-									var dec2 = decipher2.update(optionsGet[0],'hex','utf8')
-									dec2 += decipher2.final('utf8');
-
-									var decipher = crypto.createDecipher(algorithm,key)
-									var dec = decipher.update(dec2,'hex','utf8')
-									dec += decipher.final('utf8');
-									callback({verify:JSON.parse(dec)});
-									return JSON.parse(dec);
-								}else{
-									callback({error:"auth Error"});
-									return "auth Error";
-								}
-							}
-
-							if(checkTime.indexOf("s") !== -1)
-							{
-								newCheckTime = checkTime.replace("s","");
-								compare2 = compare2.add({ seconds: newCheckTime});
-								
-								if(Date.today().between(compare1,compare2))
-								{
-									var decipher2 = crypto.createDecipher(algorithm2,key)
-									var dec2 = decipher2.update(optionsGet[0],'hex','utf8')
-									dec2 += decipher2.final('utf8');
-
-									var decipher = crypto.createDecipher(algorithm,key)
-									var dec = decipher.update(dec2,'hex','utf8')
-									dec += decipher.final('utf8');
-									callback({verify:JSON.parse(dec)});
-									return JSON.parse(dec);
-								}else{
-									callback({error:"auth Error"});
-									return "auth Error";
-								}
-							}
+						}else{
+							callback({error:"auth Error"});
+							return "auth Error";
 						}
-					
+					}else{
+						callback({error:"auth Error"});
+						return "auth Error";
 					}
 				}else{
 					callback({error:"auth Error"});
@@ -225,7 +232,6 @@ try {
 				callback({error:"auth Error"});
 				return "auth Error";
 			}
-			
 		}else{
 			callback({error:"auth Error"});
 			return "auth Error";
